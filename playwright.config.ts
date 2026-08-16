8e51994d2c8bc42d03cb053badd8250e8799732b
peter.dobrosi@gmail.com <peter.dobrosi@gmail.com>
2026-08-16 22:01:30 +0200

    Update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,15 +11,10 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  ...(process.env.CI ? { workers: 1 } : {}),
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
   workers: 2,
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    trace: "on",
-  },
+  reporter: "html",
+  timeout: 60000,
+  use: { trace: "on" },
 
   /* Configure projects for major browsers */
   projects: [
